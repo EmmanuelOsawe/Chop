42a7c6c2ba89919f9a0be59c29269edfdca89cde
Your Name <emmanuelosawe90@gmail.com>
2025-09-21 16:26:24 -0700

    addreess and emails

--- eat/src/Home.js
+++ eat/src/Home.js
@@ -33,14 +33,14 @@ function Home() {
                           <div className="inner clearfix">
                               <div className="top-left clearfix">
                                   <ul className="top-info clearfix">
-                                      <li><i className="icon far fa-map-marker-alt" /> Restoria, Arrondissement, Paris 9578</li>
+                                      <li><i className="icon far fa-map-marker-alt" /> Abuja Continental 1 Ladi Kwali Street, Wuse Zone 4, </li>
                                       <li><i className="icon far fa-clock" /> Daily : 8.00 am to 10.00 pm</li>
                                   </ul>
                               </div>
                               <div className="top-right clearfix">
                                   <ul className="top-info clearfix">
                                       <li><a href="tel:+11234567890"><i className="icon far fa-phone" /> +1 123 456 7890</a></li>
-                                      <li><a href="mailto:booking@restaurant.com"><i className="icon far fa-envelope" /> booking@restaurant.com</a></li>
+                                      <li><a href="mailto:info@abujacontinental.com"><i className="icon far fa-envelope" /> info@abujacontinental.com</a></li>
                                   </ul>
                               </div>
                           </div>
@@ -60,14 +60,7 @@ function Home() {
                                   <div className="nav-outer clearfix">
                                       <nav className="main-menu">
                                           <ul className="navigation clearfix">
-                                              <li className="dropdown current"><a href="index.html#">Home</a>
-                                                  <ul>
-                                                      <li><a href="../Restoria-1-Minimal-Hero/index.html">Home 1 - Minimal Hero</a></li>
-                                                      <li><a href="index.html">Home 2 - Standard Hero</a></li>
-                                                      <li><a href="../Restoria-3-Video-Hero/index.html">Home 3 - Video Hero</a></li>
-                                                      <li><a href="../Restoria-4-Category-Hero/index.html">Home 4 - Category Hero</a></li>
-                                                      <li><a href="../Restoria-5-Rollingimages-Hero-Center-Header/index.html">Home 5 - Rolling Images</a></li>
-                                                  </ul>
+                                              <li><a href="index.html#">Home</a>
                                               </li>
                                               <li className="dropdown has-mega-menu"><a href="https://kalanidhithemes.com/live-preview/landing-page/restoria/all-demo/Restoria-2-Standard-Hero/menu-list.html">Menus</a>
                                                   <ul>
@@ -226,13 +219,13 @@ function Home() {
                       <div className="logo-box"><a href="index.html" title="Restoria - Restaurants HTML Template"><img src="/assets/images/resource/sidebar-logo.png" alt="image" title="Restoria - Restaurants HTML Template" /></a></div>
                       <h5>Visit Us</h5>
                       <ul className="info">
-                          <li>Restoria, Arrondissement, <br /> Paris 9578</li>
+                          <li>Abuja Continental <br /> 1 Ladi Kwali Street, Wuse Zone 4, </li>
                           <li> <span className="ib-ttl"> Lunch Time </span>- 10.00 am – 3:30 pm </li>
                           <li><span className="ib-ttl"> Dinner Time</span> - 08.00 pm – 10:30 pm</li>
-                          <li><a href="mailto:booking@domainame.com">booking@domainame.com</a></li>
+                          <li><a href="mailto:info@abujacontinental.com">info@abujacontinental.com</a></li>
                       </ul>
                       <div className="booking-info">
-                          <div className="bk-no"><a href="tel:+88-123-123456">Booking: +88-123-123456</a></div>
+                          <div className="bk-no"><a href="tel:+88-123-123456">Booking: +234 (0) 2094612000 / 08070602727</a></div>
                       </div>
                   </div>
               </section>
@@ -253,7 +246,7 @@ function Home() {
                   <div className="banner-call">
                       <span className="line" />
                       <ul className="call-ul">
-                          <li><a href="tel:+88-123-123456">Booking : +88-123-123456</a></li>
+                          <li><a href="tel:+88-123-123456">Booking : +234 (0) 2094612000 / 08070602727</a></li>
                       </ul>
                       <span className="line" />
                   </div>
@@ -1009,7 +1002,7 @@ function Home() {
                                       <div className="title-box centered">
                                           <div className="subtitle"><span>Online reservation</span></div>
                                           <h2>Book A Table</h2>
-                                          <div className="request-info">Booking request <a href="index.html#">+88-123-123456</a> or fill out the order form</div>
+                                          <div className="request-info">Booking request <a href="index.html#">+234 (0) 2094612000 / 08070602727</a> or fill out the order form</div>
                                       </div>
                                       <div className="default-form reservation-form">
                                           <form method="post" action="index.html">
@@ -1214,10 +1207,10 @@ function Home() {
                                           <div className="info">
                                               <h6>visit us </h6>
                                               <ul>
-                                                  <li>Restoria, Arrondissement, Paris 9578</li>
+                                                  <li>Abuja Continental 1, Ladi Kwali Street, Wuse Zone 4, </li>
                                                   <li>Daily - 8.00 am to 10.00 pm</li>
-                                                  <li><a href="mailto:booking@domainname.com">booking@gmail.com</a></li>
-                                                  <li><a href="tel:+88-123-123456">Booking Request : +88-123-123456</a></li>
+                                                  <li><a href="mailto:info@abujacontinental.com">info@abujacontinental.com</a></li>
+                                                  <li><a href="tel:+88-123-123456">Booking Request : +234 (0) 2094612000 / 08070602727</a></li>
                                               </ul>
                                           </div>
                                       </div>
